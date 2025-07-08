2646b6e0206c42124f4acaef45405b0dab17130f
MikeGraves32 <93281224+MikeGraves32@users.noreply.github.com>
2025-07-08 01:25:11 -0500

    working on css

---
+++ public/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}"
+        // "./src/**/*.{js,ts,jsx,tsx,mdx}", 
+        // If you have a src directory
+        // Add more paths here if you have them
+    ],
+    // Other configurations...,
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
